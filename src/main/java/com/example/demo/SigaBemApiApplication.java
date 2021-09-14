42bdf3a6d03f20bc19c688f92cfc9f69792f4462
Alisson Diego <diegodiniz991@gmail.com>
2021-09-14 00:00:57 -0300

    Criação do projeto

---
+++ src/main/java/com/example/demo/SigaBemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaBemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaBemApiApplication.class, args);
+	}
+
+}
